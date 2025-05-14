d3098c7a09793c11276d6c81e680e0cdb1f13fca
API Ace <srmultani018@gmail.com>
2025-05-14 22:55:11 +0530

    feat(database): implement environment-based database config - Added database property to config interface (IUsersMsConfig) and factory, for host, port, username, password, and database name via env variables

--- workspace/apps/users-ms/src/app/configuration/config.factory.ts
+++ workspace/apps/users-ms/src/app/configuration/config.factory.ts
@@ -1,23 +1,21 @@
-import type { IUsersMsConfig } from "./i-users-ms.config";
-import { generateLoggerConfig, generateSentryConfig, } from "@vanguard-nx/core";
-
-
-
-
+import { generateLoggerConfig, generateSentryConfig } from '@vanguard-nx/core';
+import { EMPTY_STR, parseInt } from '@vanguard-nx/utils';
+import type { IUsersMsConfig } from './i-users-ms.config';
+import { DEFAULT_DATABASE, DEFAULT_DB_HOST, DEFAULT_DB_PORT, DEFAULT_DB_USER } from '../db';
 
 export const configFactory = (): IUsersMsConfig => {
   const logger = generateLoggerConfig();
   const sentry = generateSentryConfig();
 
   return {
-
-
-
-
     logger,
     sentry,
-
-
-
+    database: {
+      host: process.env?.DB_HOST ?? DEFAULT_DB_HOST,
+      port: parseInt(process.env?.DB_PORT) ?? DEFAULT_DB_PORT,
+      username: process.env?.DB_USER ?? DEFAULT_DB_USER,
+      password: process.env?.DB_PASS ?? EMPTY_STR,
+      database: process.env?.DB_NAME ?? DEFAULT_DATABASE,
+    },
   };
 };
--- workspace/apps/users-ms/src/app/configuration/i-users-ms.config.ts
+++ workspace/apps/users-ms/src/app/configuration/i-users-ms.config.ts
@@ -1,10 +1,9 @@
-
-import type { Params } from "nestjs-pino";
-import type { SentryModuleOptions } from "@ntegral/nestjs-sentry";
+import type { Params } from 'nestjs-pino';
+import type { SentryModuleOptions } from '@ntegral/nestjs-sentry';
+import { IDbOptions } from '../db';
 
 export interface IUsersMsConfig {
-
   logger: Params;
   sentry: SentryModuleOptions;
-
+  database: IDbOptions;
 }
